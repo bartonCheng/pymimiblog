2c61e58ef2fcf92c42c9fa5f6331bbf4a5296e4b
Barton <barton@192.168.1.101>
2020-09-23 22:07:49 +0800

    add indexpage

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
-app.wsgi_app = ProxyFix(app.wsgi_app)
+app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
 # 倒入配置文件
 app.config.from_object('config')
 from app import routes
