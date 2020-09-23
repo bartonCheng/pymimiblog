5aafa9f8a5d32b45548e53c2aab8bace00c228fa
Barton <barton@192.168.1.101>
2020-09-23 21:57:13 +0800

    add indexpage

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,9 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 import re
+from flask import jsonify
+
+
 
 @app.route('/')
 @app.route('/index')
