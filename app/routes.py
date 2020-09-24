e7ffd6bba2e2cd8b79c5735182d545777695b709
Barton <barton@192.168.1.101>
2020-09-24 09:47:14 +0800

    add indexpage

--- app/routes.py
+++ app/routes.py
@@ -11,28 +11,28 @@ def index():
     user = {'username': 'Miguel'}
     jiekou_data = [
         {
-            'title': "vip超清视频解析【全网vip解析】",
-            'value': 'https://jx.idc126.net/jx/?url='
-        },
-        {
             'title': "①号通用vip引擎系统【稳定通用】",
-            'value': 'https://jx.598110.com/?url='
+            'value': 'http://jx.598110.com/?url='
         },
         {
             'title': "②号通用vip引擎系统【稳定通用】",
-            'value': 'https://vip.jlsprh.com/?url='
+            'value': 'http://vip.jlsprh.com/?url='
         },
         {
             'title': "③号通用vip引擎系统【稳定通用】",
-            'value': 'https://jx.618ge.com/?url='
+            'value': 'http://jx.618ge.com/?url='
         },
         {
             'title': "④号通用vip引擎系统【超级稳定通用】",
-            'value': 'https://jx.drgxj.com/?url='
+            'value': 'http://jx.drgxj.com/?url='
         },
         {
             'title': "⑤号通用vip引擎系统【全网解析】",
-            'value': 'https://jx.du2.cc/?url='
+            'value': 'http://jx.du2.cc/?url='
+        },
+        {
+            'title': "vip超清视频解析【全网vip解析】",
+            'value': 'https://jx.idc126.net/jx/?url='
         }
     ]
     # 获取ip地址
