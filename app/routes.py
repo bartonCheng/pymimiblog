98e715d487f82ac0264e67870d0b06ab68057d60
Barton <barton@192.168.1.101>
2020-09-24 09:52:10 +0800

    add indexpage

--- app/routes.py
+++ app/routes.py
@@ -19,16 +19,8 @@ def index():
             'value': 'http://vip.jlsprh.com/?url='
         },
         {
-            'title': "③号通用vip引擎系统【稳定通用】",
-            'value': 'http://jx.618ge.com/?url='
-        },
-        {
-            'title': "④号通用vip引擎系统【超级稳定通用】",
+            'title': "③号通用vip引擎系统【超级稳定通用】",
             'value': 'http://jx.drgxj.com/?url='
-        },
-        {
-            'title': "⑤号通用vip引擎系统【全网解析】",
-            'value': 'http://jx.du2.cc/?url='
         },
         {
             'title': "vip超清视频解析【全网vip解析】",
